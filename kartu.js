461edceadefc1f1a7274a385188c4064759c0ccf
Vitiarium <ikramatmaja7@gmail.com>
2025-05-05 20:59:42 +0700

    Update kartu.js

--- kartu.js
+++ kartu.js
@@ -21,6 +21,16 @@
         console.error("Error checking for updates:", error);
     }
 })();
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
 function handleYesClick() {
     window.location.href = "bunga.html";
 }
